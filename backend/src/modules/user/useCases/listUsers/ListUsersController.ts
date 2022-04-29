a984ebea87b07e3cb5dfa5590e04047389056caf
neanderdev <neander.faria@hotmail.com>
2022-04-29 16:54:11 -0300

    Making student and teacher registration and some corrections

--- backend/src/modules/user/useCases/listUsers/ListUsersController.ts
+++ backend/src/modules/user/useCases/listUsers/ListUsersController.ts
@@ -4,11 +4,19 @@ import { ListUsersUseCase } from "./ListUsersUseCase";
 
 export class ListUsersController {
   async handle(request: Request, response: Response) {
-    const { tipo_user } = request.query;
+    const { page, tipo_user } = request.query;
+    const per_page = 10;
 
     const listUsersUseCase = new ListUsersUseCase();
-    const result = await listUsersUseCase.execute(tipo_user as string);
+    const result = await listUsersUseCase.execute(
+      tipo_user as string,
+      page as string,
+      per_page
+    );
 
-    return response.json(result);
+    return response.json({
+      users: result.users,
+      totalCount: String(result.countUsers),
+    });
   }
 }
--- backend/src/modules/user/useCases/listUsers/ListUsersUseCase.ts
+++ backend/src/modules/user/useCases/listUsers/ListUsersUseCase.ts
@@ -1,13 +1,27 @@
 import { prisma } from "../../../../database/prismaClient";
 
 export class ListUsersUseCase {
-  async execute(tipo_user: string) {
+  async execute(tipo_user: string, page: string, per_page: number) {
+    const pageStart = (Number(page) - 1) * Number(per_page);
+    const pageEnd = pageStart + Number(per_page);
+
     const users = await prisma.user.findMany({
+      where: {
+        tipo_user,
+      },
+      skip: pageStart,
+      take: Number(per_page),
+    });
+
+    const totalUsers = await prisma.user.findMany({
       where: {
         tipo_user,
       },
     });
 
-    return users;
+    return {
+      users,
+      countUsers: totalUsers.length,
+    };
   }
 }
