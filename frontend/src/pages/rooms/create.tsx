747477c9dc49349d82061ad749f66b7a33774982
neanderdev <neander.faria@hotmail.com>
2022-05-03 14:42:34 -0300

    Creating page to create classes, and consuming the class creation api

---
+++ frontend/src/pages/rooms/create.tsx
@@ -0,0 +1,203 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, Stack, useColorModeValue, useMediaQuery, useToast, VStack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useMutation } from 'react-query';
+
+import { setupAPIClient } from "../../services/api";
+import { queryClient } from '../../services/queryClient';
+
+import { Navbar } from "../../components/Navbar";
+import { Sidebar } from "../../components/Sidebar";
+import { MobileSidebar } from "../../components/Sidebar/MobileSidebar";
+import { Input } from "../../components/Form/Input";
+
+type CreateClassFormData = {
+    name_class: string;
+    name_matter_class: string;
+    background_class_file?: any;
+};
+
+type UploadBackgroundClassFormData = {
+    uid_class: String;
+    data: any;
+};
+
+type GetResponseCreateClass = {
+    uid_class: string;
+    name_class: string;
+    name_matter_class: string;
+    background_class: string;
+    createdAt_class: Date;
+    updatedAt_class: Date;
+};
+
+const createClassFormSchema = yup.object().shape({
+    name_class: yup.string().required('Nome da classe obrigatório'),
+    name_matter_class: yup.string().required('Nome da matéria obrigatório'),
+    background_class_file: yup.mixed()
+        .required('A file is required')
+        // .test('isEmpty', "File is empty", (value) => value.length > 0)
+        .test('fileSize', "Arquivo é muito grande", (value) => !value[0] || (value[0] && value[0].size <= 2000000))
+        .test("type", "Aceita apenas os formatos: .png, .jpeg e .jpg", (value) => !value[0] || (value[0] && ["image/png", "image/jpeg", "image/jpg"].includes(value[0].type))),
+});
+
+export default function CreateRoom() {
+    const router = useRouter();
+    const toast = useToast();
+    const [isSmallScreen] = useMediaQuery("(max-width: 768px)");
+
+    const createClass = useMutation(async (createClass: CreateClassFormData) => {
+        const apiClient = setupAPIClient();
+        const response = await apiClient.post<GetResponseCreateClass>('class', createClass);
+
+        return response.data;
+    }, {
+        onSuccess: () => {
+            queryClient.invalidateQueries('class')
+        },
+    });
+
+    const uploadBackgroundClass = useMutation(async ({ uid_class, data }: UploadBackgroundClassFormData) => {
+        const apiClient = setupAPIClient();
+        const response = await apiClient.patch(`upload-background-class/${uid_class}`, data);
+
+        return response;
+    }, {
+        onSuccess: () => {
+            queryClient.invalidateQueries('class')
+        },
+    });
+
+    const { register, handleSubmit, formState } = useForm({
+        resolver: yupResolver(createClassFormSchema),
+    });
+
+    const { errors } = formState;
+
+    const handleCreateClass: SubmitHandler<CreateClassFormData> = async (values) => {
+        try {
+            const newClass = {
+                name_class: values.name_class,
+                name_matter_class: values.name_matter_class
+            };
+
+            const responseCreateClass = await createClass.mutateAsync(newClass);
+
+            await toast({
+                title: 'Sala de aula criado',
+                description: "Sala de aula criado com sucesso",
+                status: 'success',
+                duration: 1500,
+                isClosable: true,
+            });
+
+            if (values.background_class_file.length > 0) {
+                let data = new FormData();
+
+                data.append("class", values.background_class_file[0]);
+
+                await uploadBackgroundClass.mutateAsync({ uid_class: responseCreateClass.uid_class, data });
+            }
+
+            router.push('/rooms');
+        } catch (err) {
+            console.log(err);
+            toast({
+                title: 'Erro ao criar sala de aula',
+                description: `Erro: ${err.message}`,
+                status: 'error',
+                duration: 1500,
+                isClosable: true,
+            });
+        }
+    }
+
+    return (
+        <Box>
+            <Navbar title="Criar sala de aula" />
+
+            <Box pos="relative" h="max-content" m={[2, , 5]}>
+                <Stack direction="row" spacing={{ md: 5 }}>
+                    <Sidebar />
+
+                    {isSmallScreen && <MobileSidebar />}
+
+                    <Box w="full">
+                        <Box
+                            as='form'
+                            flex='1'
+                            borderRadius={8}
+                            bg={useColorModeValue('gray.200', 'gray.800')}
+                            p={['6', '8']}
+                            onSubmit={handleSubmit(handleCreateClass)}
+                        >
+                            <Heading size='lg' fontWeight='normal'>Criar sala de aula</Heading>
+
+                            <Divider my='6' borderColor='gray.700' />
+
+                            <VStack spacing='8'>
+
+                                <SimpleGrid minChildWidth='240px' spacing={['6', '8']} w='100%'>
+                                    <Input
+                                        name='name_class'
+                                        label='Nome da classe'
+                                        {...register('name_class')}
+                                        error={errors.name_class}
+                                        bgColor={useColorModeValue('gray.50', 'gray.600')}
+                                        _hover={{
+                                            bgColor: useColorModeValue('gray.100', 'gray.500')
+                                        }}
+                                        _focus={{
+                                            bg: useColorModeValue('gray.100', 'gray.500'),
+                                        }}
+                                    />
+                                    <Input
+                                        name='name_matter_class'
+                                        label='Nome da matéria'
+                                        {...register('name_matter_class')}
+                                        error={errors.name_matter_class}
+                                        bgColor={useColorModeValue('gray.50', 'gray.600')}
+                                        _hover={{
+                                            bgColor: useColorModeValue('gray.100', 'gray.500')
+                                        }}
+                                        _focus={{
+                                            bg: useColorModeValue('gray.100', 'gray.500'),
+                                        }}
+                                    />
+                                    <Input
+                                        name='background_class_file'
+                                        type="file"
+                                        label='Background da turma'
+                                        {...register('background_class_file')}
+                                        error={errors.background_class_file}
+                                        bgColor={useColorModeValue('gray.50', 'gray.600')}
+                                        _hover={{
+                                            bgColor: useColorModeValue('gray.100', 'gray.500')
+                                        }}
+                                        _focus={{
+                                            bg: useColorModeValue('gray.100', 'gray.500'),
+                                        }}
+                                        accept="image/*"
+                                    />
+                                </SimpleGrid>
+                            </VStack>
+
+                            <Flex mt='8' justify='flex-end'>
+                                <HStack spacing='4'>
+                                    <Link href='/rooms' passHref>
+                                        <Button colorScheme='whiteAlpha'>Cancelar</Button>
+                                    </Link>
+
+                                    <Button type='submit' colorScheme='pink' isLoading={formState.isSubmitting}>Salvar</Button>
+                                </HStack>
+                            </Flex>
+                        </Box>
+                    </Box>
+                </Stack>
+            </Box>
+        </Box>
+    );
+}
--- frontend/src/pages/rooms/index.tsx
+++ frontend/src/pages/rooms/index.tsx
@@ -1,4 +1,5 @@
-import { Box, SimpleGrid, Stack, useMediaQuery } from "@chakra-ui/react";
+import { Box, Button, Icon, SimpleGrid, Stack, useMediaQuery } from "@chakra-ui/react";
+import { RiAddLine } from "react-icons/ri";
 
 import { withSSRAuth } from "../../utils/withSSRAuth";
 
@@ -6,6 +7,7 @@ import { Navbar } from "../../components/Navbar";
 import { Sidebar } from "../../components/Sidebar";
 import { MobileSidebar } from "../../components/Sidebar/MobileSidebar";
 import { ClassCard } from "../../components/ClassCard";
+import { Can } from "../../components/Can";
 
 export default function Rooms() {
     const [isSmallScreen] = useMediaQuery("(max-width: 768px)");
@@ -21,6 +23,20 @@ export default function Rooms() {
                     {isSmallScreen && <MobileSidebar />}
 
                     <Box w="full">
+                        <Can roles="admin">
+                            <Button
+                                as='a'
+                                size='sm'
+                                fontSize='sm'
+                                colorScheme='purple'
+                                leftIcon={<Icon as={RiAddLine} fontSize='24' />}
+                                mb="4"
+                                href="/rooms/create"
+                            >
+                                Criar turma
+                            </Button>
+                        </Can>
+
                         <SimpleGrid flex='1' gap='4' minChildWidth='320px' alignItems='flex-start'>
                             <ClassCard
                                 imageClass="https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=100"
