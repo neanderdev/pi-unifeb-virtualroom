36921a82e80bbe12b40a1dfa3e355213e4032826
neanderdev <neander.faria@hotmail.com>
2022-05-25 16:08:43 -0300

    Consuming the Grade API on Student Activity

--- frontend/src/components/StudentGrades/StudentGradesDetail.tsx
+++ frontend/src/components/StudentGrades/StudentGradesDetail.tsx
@@ -1,21 +1,48 @@
 import { useState } from "react";
-import { Flex, FormControl, FormErrorMessage, Icon, IconButton, Input, Text, useColorModeValue } from "@chakra-ui/react";
+import { Flex, FormControl, FormErrorMessage, Icon, IconButton, Input, Text, useColorModeValue, useToast } from "@chakra-ui/react";
 import { IoSaveSharp } from "react-icons/io5";
+import { useMutation } from "react-query";
+
+import { setupAPIClient } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
 
 interface StudentGradesDetailProps {
+    activity_uid: string;
+    id_detail_activity: number | undefined;
     uid_user: string;
     name_user: string;
+    nota_user: string | undefined;
     nota_max_activity: number;
     isStudentDeliveredTheActivity: boolean;
 };
 
 interface ErrorInputNote {
     message: string;
-}
+};
 
-export function StudentGradesDetail({ uid_user, name_user, nota_max_activity, isStudentDeliveredTheActivity }: StudentGradesDetailProps) {
-    const [note, setNote] = useState(null);
+interface GradeStudents {
+    id_detail_activity: number | null;
+    nota_user: number | string;
+    activity_uid: string;
+    user_uid: string;
+};
+
+export function StudentGradesDetail({ activity_uid, id_detail_activity, uid_user, name_user, nota_user, nota_max_activity, isStudentDeliveredTheActivity }: StudentGradesDetailProps) {
+    const toast = useToast();
+
+    const [note, setNote] = useState(nota_user === undefined ? null : nota_user);
     const [error, setError] = useState<ErrorInputNote | null>({} as ErrorInputNote | null);
+
+    const gradeTheStudent = useMutation(async (gradeStudents: GradeStudents) => {
+        const apiClient = setupAPIClient();
+        const response = await apiClient.post('grade-the-student', gradeStudents);
+
+        return response;
+    }, {
+        onSuccess: () => {
+            queryClient.invalidateQueries('listActivityAndUsers')
+        },
+    });
 
     const handleChange = (event) => {
         if (event.target.value <= nota_max_activity) {
@@ -31,6 +58,37 @@ export function StudentGradesDetail({ uid_user, name_user, nota_max_activity, is
         } else {
             setError({
                 message: null,
+            });
+        }
+    };
+
+    const handleGradeTheStudent = async () => {
+        try {
+            const gradeTheStudents = {
+                id_detail_activity: id_detail_activity === undefined ? null : id_detail_activity,
+                nota_user: note,
+                activity_uid: activity_uid,
+                user_uid: uid_user,
+            };
+
+            await gradeTheStudent.mutateAsync(gradeTheStudents);
+
+            await toast({
+                title: 'Dar nota ao aluno',
+                description: "Dar nota ao aluno com sucesso",
+                status: 'success',
+                duration: 1500,
+                isClosable: true,
+            });
+        } catch (err) {
+            console.log(err);
+
+            toast({
+                title: 'Erro ao dar nota ao aluno',
+                description: `Erro: ${err.message}`,
+                status: 'error',
+                duration: 1500,
+                isClosable: true,
             });
         }
     };
@@ -107,7 +165,7 @@ export function StudentGradesDetail({ uid_user, name_user, nota_max_activity, is
                     }}
                     icon={<Icon as={IoSaveSharp} fontSize={18} />}
                     disabled={(note === null || note === "") && true}
-                    onClick={() => alert("Nota: " + note)}
+                    onClick={handleGradeTheStudent}
                 />
             </Flex>
         </Flex>
--- frontend/src/components/StudentGrades/index.tsx
+++ frontend/src/components/StudentGrades/index.tsx
@@ -5,62 +5,6 @@ import { useListActivityAndUsers } from "../../services/hooks/useListActivityAnd
 import { StudentGradesHeader } from "./StudentGradesHeader";
 import { StudentGradesDetail } from "./StudentGradesDetail";
 
-interface User {
-    id_user: number;
-    name_user: string;
-};
-
-interface FakeApiStudentActivity {
-    id_activity: number;
-    name_activity: string;
-    nota_max: number;
-    data_entrega_activity: Date | string;
-    user: User[];
-};
-
-const fakeApiStudentActivity: FakeApiStudentActivity[] = [
-    {
-        id_activity: 1,
-        name_activity: "TDE 01",
-        nota_max: 4,
-        data_entrega_activity: new Date(),
-        user: [
-            {
-                id_user: 1,
-                name_user: "Neander de Souza Faria"
-            },
-            {
-                id_user: 2,
-                name_user: "Vinicius Cardoso"
-            },
-            {
-                id_user: 3,
-                name_user: "Dudu Buch"
-            },
-        ],
-    },
-    {
-        id_activity: 2,
-        name_activity: "TDE 02",
-        nota_max: 1,
-        data_entrega_activity: new Date(),
-        user: [
-            {
-                id_user: 1,
-                name_user: "Neander de Souza Faria"
-            },
-            {
-                id_user: 2,
-                name_user: "Vinicius Cardoso"
-            },
-            {
-                id_user: 3,
-                name_user: "Dudu Buch"
-            },
-        ],
-    },
-];
-
 interface StudentGradesProps {
     class_uid: string;
 }
@@ -108,8 +52,11 @@ export function StudentGrades({ class_uid }: StudentGradesProps) {
                         {activity.class?.ClassUser?.map((user) => (
                             <StudentGradesDetail
                                 key={user.user.uid_user}
+                                activity_uid={activity.uid_activity}
+                                id_detail_activity={activity.DetailActivity.filter((detail) => detail.user_uid === user.user.uid_user)[0]?.id_detail_activity}
                                 uid_user={user.user.uid_user}
                                 name_user={user.user.name_user}
+                                nota_user={activity.DetailActivity.filter((detail) => detail.user_uid === user.user.uid_user)[0]?.nota_user}
                                 nota_max_activity={activity.nota_max_activity}
                                 isStudentDeliveredTheActivity={activity.DetailActivity.filter((detail) => detail.user_uid === user.user.uid_user).length > 0 ? true : false}
                             />
